e39b4fa56cff0f7d8daf685b6437ee924dc43bf3
kayaktavist <imagerybyar@gmail.com>
2019-02-04 13:21:49 -0700

    adding ticket info api

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&order=rating&type=video&videoDefinition=high&videoEmbeddable=true&key=AIzaSyDlDeXz6hox05IplaBivh2Owr3tnDzfFaE";
 
 // .on("click") function associated with the Search Button
-$(".btn").on("click", function(event) {
+$(".btn").on("click", function (event) {
   // This line allows us to take advantage of the HTML "submit" property
   // This way we can hit enter on the keyboard and it registers the search
   // (in addition to clicks). Prevents the page from reloading on form submit.
@@ -14,12 +14,12 @@ $(".btn").on("click", function(event) {
 
   // Make the AJAX request to the API - GETs the JSON data at the queryURL.
   // The data then gets passed as an argument to the updatePage function
-  
+
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response){
-      console.log(response)
+  }).then(function (response) {
+    console.log(response)
   }
   )
 });
@@ -31,33 +31,67 @@ $("#clear-all").on("click", clear);
 function artistInfo(artists) {
 
   var queryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artists + "&api_key=24b4aa93fb9c29dedfbb4977c6bb8cf4&format=json"
-  
+
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
-  console.log(response);
+  }).then(function (response) {
+    console.log(response);
     var artistName = $("<h1>").text(response.artist.name);
     var artistURL = $("<a>").attr(response.artist).append(artistName);
     var artistBio = $("<h3>").text(response.artist.bio.summary);
     var artistImage = $("<img>").attr("src", response.artist.image[3]["#text"]);
     var upcomingEvents = $("<h4>").text(response.artist.ontour + " Upcoming Events");
-    var goToArtist = $("<a>").attr("href", response.artist.url).text("See Tour Dates"); 
+    var goToArtist = $("<a>").attr("href", response.artist.url).text("See Tour Dates");
 
     // Empty the contents of the artist-div, append the new artist content
     $("#artist-div").empty();
-    $("#artist-div").append(artistURL, artistImage, artistBio, upcomingEvents,goToArtist);
+    $("#artist-div").append(artistURL, artistImage, artistBio, upcomingEvents, goToArtist);
   });
 }
 
 // Event handler for user clicking the select-artist button
-$("#submit").on("click", function(event) {
+$("#submit").on("click", function (event) {
   event.preventDefault();
   var inputArtist = $("#search").val().trim();
 
   artistInfo(inputArtist);
-
+  concertInfo(inputArtist)
 });
 
 
+function concertInfo(artists) {
+
+  var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + artists + "&apikey=JRYkzb07x4RvvpOyhVRJUcNJnlRoMffq"
+
+
+  $.ajax({
+    type: "GET",
+  url: queryURL,
+  async: true,
+  dataType: "json",
+    success: function (json) {
+      console.log(json);
+      // Parse the response.
+      // Do other things.
+    },
+    error: function (xhr, status, err) {
+      // This time, we do not end up here!
+    }
+  }).then(function (response) {
+    console.log(response);
+    $("#cBlockOne").empty();
+
+    for (var i = 0; i < ressponse._embedded.events.length; i++) {
+    
+    var eventName = $("<h1>").text(response._embedded.events.name);
+    var eventURL = $("<a>").attr(response._embedded.events.url).append(artistName);
+    var eventDates = $("<h4>").text(response._embedded.dates + " Upcoming Events");
+    var eventCity = $("h4").text((response._embedded.events._embedded.venues.city));
+    
+    
+    $("#cBlockOne").append(eventName, eventURL, eventDates, eventCity);
+    }
+  });
+}
 
