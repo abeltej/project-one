8c0cf79c36001bd1d733a3a88b5ca1a749ad947e
kayaktavist <imagerybyar@gmail.com>
2019-02-06 13:24:12 -0700

    cleaned up files

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 $(document).ready(function () {
-
   $('.vidWindow').hide();
-
- 
 });
 
+// Event handler for user clicking the search-artist button
+$("#submit").on("click", function (event) {
+  event.preventDefault();
+  var inputArtist = $("#search").val().trim();
 
+  var validation = validator.isEmpty(inputArtist);
 
+  // if text is entered into search box, perform api call funcitons and show video window
+  if (validation === false) {
+    $('.vidWindow').show();
+    artistInfo(inputArtist);
+    concertInfo(inputArtist);
+    youtubeArtist(inputArtist);
+  }
+});
+
+// youtube api call funciton
 function youtubeArtist(keyword) {
 
   var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + keyword + "&type=video&key=AIzaSyCFdNzrcERfUCd1R5rk1h3JAjXNT9svi5g"
@@ -15,23 +27,16 @@ function youtubeArtist(keyword) {
     url: queryURL,
     method: "GET"
   }).then(function (response) {
-    console.log(response);
-    // var videoURl = response.items[0].id.videoId;
-    // var newVideo = $("<iframe>").attr('src', videoURL)
-
-    console.log(response.items[0].snippet.title);
+    // console.log(response);
+    // console.log(response.items[0].snippet.title);
 
     var videoId = response.items[0].id.videoId;
     $("#testing").attr("src", "https://www.youtube.com/embed/" + videoId);
-    console.log(response);
-
+    // console.log(response);
   });
 }
 
-  
-
-
-
+// api call function to LastFM for arist info and image
 function artistInfo(artists) {
 
   var queryURL = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artists + "&api_key=24b4aa93fb9c29dedfbb4977c6bb8cf4&format=json"
@@ -51,30 +56,10 @@ function artistInfo(artists) {
   });
 }
 
-// Event handler for user clicking the select-artist button
-$("#submit").on("click", function (event) {
-  event.preventDefault();
-  var inputArtist = $("#search").val().trim();
-
-  var validation = validator.isEmpty(inputArtist);
-
-  if (validation === false) {
-    $('.vidWindow').show();
-    artistInfo(inputArtist);
-    concertInfo(inputArtist);
-    youtubeArtist(inputArtist);
-    
-    console.log("true");
-  }
-
-
-});
-
-
+// api call funciton to ticketmaster for conert info
 function concertInfo(artists) {
 
   var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + artists + "&apikey=JRYkzb07x4RvvpOyhVRJUcNJnlRoMffq"
-
 
   $.ajax({
     type: "GET",
@@ -83,11 +68,11 @@ function concertInfo(artists) {
     dataType: "json",
 
   }).then(function (response) {
-    console.log(response);
+    // console.log(response);
     $("#concerts").empty();
 
     for (var i = 0; i++ < 4;) {
-      console.log("loop")
+      // console.log("loop")
 
       var eventCity = $("<h5>").text(response._embedded.events[i]._embedded.venues[0].city.name);
       var eventDates = $("<h6>").text(response._embedded.events[i].dates.start.localDate);
